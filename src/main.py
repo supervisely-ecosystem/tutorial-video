71e9dcbc27d0504d9719c79feced1ba78644ee9d
almaz_macbook <almazgimaev@gmail.com>
2022-12-23 11:37:56 +0300

    text edited

--- src/main.py
+++ src/main.py
@@ -15,22 +15,22 @@ workspace_id = sly.env.workspace_id()
 original_dir = "src/videos/original"
 result_dir = "src/videos/result"
 
-# Remove the results directory if it exists.
+# Remove results directory if it exists.
 if os.path.exists(result_dir):
     rmtree(result_dir)
 os.mkdir(result_dir)
 
-# Create a new Supervisely project.
+# Create new Supervisely project.
 project = api.project.create(
     workspace_id, "Animals", type=ProjectType.VIDEOS, change_name_if_conflict=True
 )
 print(f"Project ID: {project.id}")
 
-# Create a new Supervisely dataset.
+# Create new Supervisely dataset.
 dataset = api.dataset.create(project.id, "Birds")
 print(f"Dataset ID: {dataset.id}")
 
-# Upload the video from the local directory to the Supervisely platform.
+# Upload video from local directory to Supervisely platform.
 path = os.path.join(original_dir, "Penguins.mp4")
 meta = {"my-field-1": "my-value-1", "my-field-2": "my-value-2"}
 
@@ -38,40 +38,40 @@ video = api.video.upload_path(
     dataset.id,
     name="Penguins",
     path=path,
-    meta=meta,  # optional: you can add metadata to the video.
+    meta=meta,  # optional: you can add metadata to video.
 )
-print(f'Video "{video.name}" uploaded to the Supervisely platform with ID:{video.id}')
+print(f'Video "{video.name}" uploaded to Supervisely platform with ID:{video.id}')
 
 
-# Upload a list of videos from the local directory to the Supervisely platform.
+# Upload list of videos from local directory to Supervisely platform.
 names = ["Flamingo.mp4", "Swans.mp4", "Toucan.mp4"]
 paths = [os.path.join(original_dir, name) for name in names]
 
-# This method uses fewer requests to the database and allows to upload more videos efficiently.
+# This method uses fewer requests to database and allows to upload more videos efficiently.
 upload_info = api.video.upload_paths(dataset.id, names, paths)
-print(f"{len(upload_info)} videos successfully uploaded to the Supervisely platform")
+print(f"{len(upload_info)} videos successfully uploaded to Supervisely platform")
 
 
-# Get information about the video from Supervisely by id.
+# Get information about video from Supervisely by id.
 video_info = api.video.get_info_by_id(video.id)
 print(video_info)
 
-# Get information about the video from Supervisely by name and dataset ID.
+# Get information about video from Supervisely by name and dataset ID.
 video_info_by_name = api.video.get_info_by_name(dataset.id, video.name)
 print(f"Video name - {video_info_by_name.name}")
 
-# Get a list of all the videos from the Supervisely dataset.
+# Get list of all videos from Supervisely dataset.
 video_info_list = api.video.get_list(dataset.id)
 print(f"{len(video_info_list)} videos information received.")
 
 
-# Download the video from the Supervisely platform to the local directory by id.
+# Download video from Supervisely platform to local directory by id.
 save_path = os.path.join(result_dir, f"{video_info.name}.mp4")
 api.video.download_path(video_info.id, save_path)
 print(f"Video has been successfully downloaded to '{save_path}'")
 
 
-# Download the frame of the video as an image from the Supervisely platform to the local directory.
+# Download frame of video from Supervisely platform as image and save to local directory.
 frame_idx = 15
 file_name = "frame.png"
 save_path = os.path.join(result_dir, file_name)
@@ -79,30 +79,30 @@ api.video.frame.download_path(video_info.id, frame_idx, save_path)
 print(f"Video frame has been successfully downloaded as an image to '{save_path}'")
 
 
-# You can also download the video frame in RGB NumPy matrix format.
+# You can also download video frame as RGB NumPy matrix.
 video_frame_np = api.video.frame.download_np(video_info.id, frame_idx)
 print(f"Video frame downloaded in RGB NumPy matrix. Frame shape: {video_frame_np.shape}")
 
 
-# Download the range of video frames as images from the Supervisely platform to the local directory
+# Download multiple frames from Supervisely platform as images and save to local directory
 frame_indexes = [5, 10, 20, 30, 45]
 save_paths = [os.path.join(result_dir, f"frame_{idx}.png") for idx in frame_indexes]
 
-# This method uses fewer requests to the database and downloads more videos efficiently.
+# This method uses fewer requests to database and downloads more videos efficiently.
 api.video.frame.download_paths(video_info.id, frame_indexes, save_paths)
 print(f"{len(frame_indexes)} images has been successfully downloaded to '{save_path}'")
 
 
-# Download the range of video frames in RGB NumPy matrix format from the Supervisely platform.
+# Download range of video frames as RGB NumPy matrix from Supervisely platform.
 video_frames_np = api.video.frame.download_nps(video_info.id, frame_indexes)
 print(f"{len(video_frames_np)} video frames downloaded in RGB NumPy matrix.")
 
-# Remove video from the Supervisely platform by id
+# Remove video from Supervisely platform by id
 api.video.remove(video_info.id)
 print(f"Video (ID: {video_info.id}) successfully removed")
 
 
-# Remove the list of the videos from the Supervisely platform by ids
+# Remove list of videos from Supervisely platform by ids
 videos_to_remove = api.video.get_list(dataset.id)
 remove_ids = [video.id for video in videos_to_remove]
 api.video.remove_batch(remove_ids)
